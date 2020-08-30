f7592982fbc5c007d29e2ecbc2e91e6f893dffdc
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-08-29 21:30:30 -0500

    Implement DTO to UserController

--- Catalogue.Api/Controllers/UsersController.cs
+++ Catalogue.Api/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalogue.Core.DTOs;
 using Catalogue.Core.Entities;
 using Catalogue.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -18,26 +19,51 @@ namespace Catalogue.Api.Controllers
         public UsersController(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-
         }
 
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
             var users = await _userRepository.GetUsers();
-            return Ok(users);
+            var usersDTO = users.Select(u => new UserDTO
+            {
+                Id = u.Id,
+                Username = u.Username,
+                Token = u.Token,
+                UpdatedAt = u.UpdatedAt,
+                CreatedAt = u.CreatedAt
+            });
+
+            return Ok(usersDTO);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _userRepository.GetUser(id);
-            return Ok(user);
+            var userDTO = new UserDTO
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Token = user.Token,
+                UpdatedAt = user.UpdatedAt,
+                CreatedAt = user.CreatedAt
+            };
+
+            return Ok(userDTO);
         }
 
         [HttpPost]
-        public async Task<IActionResult> addUser(User user)
+        public async Task<IActionResult> addUser(UserDTO userDTO)
         {
+            var user = new User
+            {
+                Username = userDTO.Username,
+                Token = userDTO.Token,
+                UpdatedAt = userDTO.UpdatedAt,
+                CreatedAt = userDTO.CreatedAt
+            };
+
             await _userRepository.addUser(user);
             return Ok(user);
         }
