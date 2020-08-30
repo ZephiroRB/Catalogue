68491b1da32d9cde2f159c89a8f74ae7d749d31d
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-08-30 00:58:35 -0500

    Add AutoMapperProfile

---
+++ Catalogue.Infrastructure/Mappings/AutomapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Catalogue.Core.DTOs;
+using Catalogue.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Infrastructure.Mappings
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+        }
+    }
+}
