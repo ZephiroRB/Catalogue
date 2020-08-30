3ef4eefa30c5cf9b9b65ca69d9e97e82d4a2511e
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-08-29 21:28:52 -0500

    Add UserDTO

---
+++ Catalogue.Core/DTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Core.DTOs
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
