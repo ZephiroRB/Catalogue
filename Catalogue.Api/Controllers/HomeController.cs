f253d497c765f0d081ba7a0bc07b7a1d0b3ea970
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-08-31 17:15:51 -0500

    Add Root ApI

---
+++ Catalogue.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogue.Api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello from .NET core");
+        }
+    }
+}
