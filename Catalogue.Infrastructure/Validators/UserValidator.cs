24cd769c03a908bd7af1d537c3901e710f8838f7
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-03 02:37:46 -0500

    Add UserValidator

---
+++ Catalogue.Infrastructure/Validators/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using Catalogue.Core.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Infrastructure.Validators
+{
+    public class UserValidator : AbstractValidator<UserDTO>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Username)
+                .NotNull();
+
+            RuleFor(user => user.Password)
+                .NotNull();
+        }
+    }
+}
