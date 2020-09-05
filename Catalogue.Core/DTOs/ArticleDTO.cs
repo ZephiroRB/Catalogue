a7cf53a2a2263112b837bc1d36236ba37a3c7da6
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-05 02:29:22 -0500

    Add Article DTO

---
+++ Catalogue.Core/DTOs/ArticleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Core.DTOs
+{
+    public class ArticleDTO
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string Token { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
