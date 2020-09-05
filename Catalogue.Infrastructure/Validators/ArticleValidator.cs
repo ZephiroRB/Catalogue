4c0eed677fa0f8e348f2a7a6ecee0fbd1a533fd0
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-05 02:28:23 -0500

    Add Validator Article

---
+++ Catalogue.Infrastructure/Validators/ArticleValidator.cs
@@ -0,0 +1,18 @@
+﻿using Catalogue.Core.DTOs;
+using FluentValidation;
+
+namespace Catalogue.Infrastructure.Validators
+{
+    public class ArticleValidator: AbstractValidator<ArticleDTO>
+    {
+        public ArticleValidator()
+        {
+            RuleFor(article => article.Title)
+               .NotNull();
+
+            RuleFor(article => article.UserId)
+                    .NotNull();
+        }
+       
+    }
+}
