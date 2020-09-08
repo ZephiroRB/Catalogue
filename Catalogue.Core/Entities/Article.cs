75825b6b9c55a9bc6450d3ee39756b2d8ba7a997
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-08 01:40:13 -0500

    Add BaseEntity and remove fields

--- Catalogue.Core/Entities/Article.cs
+++ Catalogue.Core/Entities/Article.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 
 namespace Catalogue.Core.Entities
 {
-    public partial class Article
+    public partial class Article : BaseEntity
     {
-        public long Id { get; set; }
         public long UserId { get; set; }
         public string Token { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+       
 
         public virtual User User { get; set; }
     }
--- Catalogue.Core/Entities/User.cs
+++ Catalogue.Core/Entities/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Catalogue.Core.Entities
 {
-    public partial class User
+    public partial class User : BaseEntity
     {
 
         public User()
@@ -11,12 +11,10 @@ namespace Catalogue.Core.Entities
             Articles = new HashSet<Article>();
         }
 
-        public long Id { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+
 
         public virtual ICollection<Article> Articles { get; set; }
     }
