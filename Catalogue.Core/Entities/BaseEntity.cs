0bf255c28159f08c0fe2b1da3b28560a710fcba6
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-08 01:37:19 -0500

    Add Base Entity

---
+++ Catalogue.Core/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
