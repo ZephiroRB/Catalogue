4aef7d5f70122b9bef4816d9d87f437e64a878a5
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-08 01:38:05 -0500

    Add IRepository Generic with Crud Actions

---
+++ Catalogue.Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalogue.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue.Core.Interfaces
+{
+    public interface IRepository<T> where  T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(long id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(long id);
+
+    }
+}
