119e89133e1e07296506c62e483c22b0340d7ddc
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-10-02 01:22:26 -0500

    Update Article Validator

--- Catalogue.Infrastructure/Validators/ArticleValidator.cs
+++ Catalogue.Infrastructure/Validators/ArticleValidator.cs
@@ -1,17 +1,22 @@
 ﻿using Catalogue.Core.DTOs;
 using FluentValidation;
 
+
 namespace Catalogue.Infrastructure.Validators
 {
     public class ArticleValidator: AbstractValidator<ArticleDTO>
     {
         public ArticleValidator()
         {
+            RuleFor(article => article.UserId)
+                    .NotNull()
+                    .NotEmpty();
+
             RuleFor(article => article.Title)
                .NotNull();
 
-            RuleFor(article => article.UserId)
-                    .NotNull();
+            RuleFor(article => article.Description)
+               .NotNull();
         }
        
     }
